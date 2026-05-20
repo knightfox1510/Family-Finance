9cf883613346764cc919cb508b6ac7bef594151c
knightfox1510 <42672253+knightfox1510@users.noreply.github.com>
2026-05-20 12:50:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5456,7 +5456,9 @@ export default function App() {
                 });
                 setPrevView(view);
                 setView('add');
-            }
+            }} 
+            />
+          )}
               
           {view === 'settle' && (
             <SettleDashboard data={data} onBulkSettle={actions.bulkSettle} />
