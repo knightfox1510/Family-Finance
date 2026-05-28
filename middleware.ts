06320f67d1e9541190892b0dac1733a1cdaa35e2
knightfox1510 <42672253+knightfox1510@users.noreply.github.com>
2026-05-28 11:45:32 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -10,19 +10,19 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          );
-          supabaseResponse = NextResponse.next({ request });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          );
-        },
-      },
+  getAll() {
+    return request.cookies.getAll();
+  },
+  setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
+    cookiesToSet.forEach(({ name, value }) =>
+      request.cookies.set(name, value)
+    );
+    supabaseResponse = NextResponse.next({ request });
+    cookiesToSet.forEach(({ name, value, options }) =>
+      supabaseResponse.cookies.set(name, value, options)
+    );
+  },
+},
     }
   );
 
