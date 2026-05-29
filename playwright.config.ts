72a525391ae1958b72694ade00fd845606aaa20e
knightfox1510 <42672253+knightfox1510@users.noreply.github.com>
2026-05-29 20:21:29 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forgetOnAnomalies: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: true,
+    timeout: 120 * 1000,
+  },
+});
